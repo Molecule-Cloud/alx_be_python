f688dd10ed2a1d1071f51032722c3492d1d41a33
Benjamin Appiah <ben.appiah451@gmail.com>
2025-11-09 21:02:38 +0000

    Correct calculation for monthly savings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
 monthly_income = int(input("Enter your monthly income: "))
-total_monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 annual_interest_rate = 0.05
 
 # Calculate Monthly savings and projected annual savigs.
-monthly_savings = monthly_income - total_monthly_expenses
+monthly_savings = (monthly_income - monthly_expenses)
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print(f"Your monthly savings are ${monthly_savings}")
