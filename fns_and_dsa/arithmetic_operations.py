187dc822a2a923ea370100faa94c26b20d81a458
Benjamin Appiah <ben.appiah451@gmail.com>
2025-11-23 20:27:02 +0000

    Change match case to if-else for arithmetric.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,9 +6,10 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        try:
-            return num1 / num2
-        except ZeroDivisionError:
+        if num2 == 0:
             return "Division by zero is not allowed"
+        else:
+            return num1 / num2
+            
     else:
         return "Error"
